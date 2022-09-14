cf894ed9f1ada918608b68a6b6d8737f14094843
Samuel Adu <theadusamuel@gmail.com>
2022-09-14 19:58:12 +0100

    add homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,20 @@
 function Home() {
   return (
-    <div>
-      <h1>So, you want to travel to space</h1>
-      <p>
-        Let’s face it; if you want to go to space, you might as well genuinely
-        go to outer space and not hover kind of on the edge of it. Well sit
-        back, and relax because we’ll give you a truly out of this world
-        experience!
-      </p>
+    <div className="">
+      <section>
+        <h1 className="heading">
+          <span>So, you want to travel to</span> space
+        </h1>
 
-      <button className="btn">Explore</button>
+        <p className="body-text">
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+
+        <button className="btn">Explore</button>
+      </section>
     </div>
   );
 }
